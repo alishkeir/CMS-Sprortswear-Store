8d5371609ee8788aa4db302d9a2d9f02f359d1ae
Ali Shkeir <ali_shkeir1996@hotmail.com>
2021-01-30 09:01:47 +0200

    Edited Controllers Names

--- back/app/Http/Controllers/AdminsController.php
+++ back/app/Http/Controllers/AdminsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Categories extends Controller
+class AdminsController extends Controller
 {
     /**
      * Display a listing of the resource.
--- back/app/Http/Controllers/ItemsController.php
+++ back/app/Http/Controllers/ItemsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Items extends Controller
+class ItemsController extends Controller
 {
     /**
      * Display a listing of the resource.
